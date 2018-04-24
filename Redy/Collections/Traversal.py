d3f7db65affbbe5618a85a08f8b7900aeafcd6d7
thautwarm <twshere@outlook.com>
2018-04-25 00:07:35 +0800

    Collections.Traversal: add `chunk_by`, `chunk`, `group_by`, `group`

--- Redy/Collections/Traversal.py
+++ Redy/Collections/Traversal.py
@@ -1,8 +1,10 @@
 import builtins
 from ..Typing import *
 import functools
+from collections import defaultdict
 
-__all__ = ['map_by', 'reduce_by', 'fold_by', 'sum_from', 'each_do', 'filter_by', 'flatten_to', 'flatten_if']
+__all__ = ['map_by', 'reduce_by', 'fold_by', 'sum_from', 'each_do', 'filter_by', 'flatten_to', 'flatten_if', 'chunk_by',
+           'chunk', 'group', 'group_by']
 
 ActualIterable = Union[Iterable, Iterable, Sequence, Collection]
 
@@ -128,3 +130,90 @@ def flatten_if(cond: Callable[[Union[T, ActualIterable[T]]], bool]):
     return inner
 
 
+def chunk_by(fn: Callable[[T], object]):
+    """
+    >>> from Redy.Collections import Traversal, Flow
+    >>> lst: Iterable[int] = [0, 1, 2, 3, 4, 5, 6]
+    >>> x = Flow(lst)[Traversal.chunk_by(lambda x: x // 3)]
+    >>> assert list(x.unbox) == [[0, 1, 2], [3, 4, 5], [6]]
+    >>> x = Flow([])[Traversal.chunk_by(lambda x: x)]
+    >>> assert list(x.unbox) == []
+    """
+
+    def inner(seq: ActualIterable[T]) -> ActualIterable[ActualIterable[T]]:
+        seq = iter(seq)
+        try:
+            head = next(seq)
+        except StopIteration:
+            return iter(seq)
+
+        current_status = fn(head)
+        group = [head]
+        for each in seq:
+            status = fn(each)
+            if status != current_status:
+                yield group
+                group = [each]
+            else:
+                group.append(each)
+            current_status = status
+        if group:
+            yield group
+
+    return inner
+
+
+def chunk(seq: ActualIterable[T]) -> ActualIterable[ActualIterable[T]]:
+    """
+    >>> from Redy.Collections import Traversal, Flow
+    >>> x = [1, 1, 2]
+    >>> assert Flow(x)[Traversal.chunk][list].unbox == [[1, 1], [2]]
+    >>> assert Flow([])[Traversal.chunk][list].unbox == []
+    """
+    seq = iter(seq)
+    try:
+        head = next(seq)
+    except StopIteration:
+        return iter(seq)
+
+    current_status = head
+    group = [head]
+    for each in seq:
+        status = each
+        if status != current_status:
+            yield group
+            group = [each]
+        else:
+            group.append(each)
+        current_status = status
+    if group:
+        yield group
+
+
+def group_by(fn: Callable[[T], TR]):
+    """
+    >>> from Redy.Collections import Flow, Traversal
+    >>> x = [1, '1', 1.0]
+    >>> Flow(x)[Traversal.group_by(type)]
+    """
+
+    def inner(seq: ActualIterable[T]) -> Dict[TR, List[T]]:
+        ret = defaultdict(list)
+        for each in seq:
+            ret[fn(each)].append(each)
+
+        return ret
+
+    return inner
+
+
+def group(seq: ActualIterable[T]) -> Dict[TR, List[T]]:
+    """
+    >>> from Redy.Collections import Flow, Traversal
+    >>> x = [1, 1, 2]
+    >>> Flow(x)[Traversal.group].unbox
+    """
+    ret = defaultdict(list)
+    for each in seq:
+        ret[each].append(each)
+    return ret
--- tests/Collections/Traversal.py
+++ tests/Collections/Traversal.py
@@ -47,3 +47,23 @@ from Redy.Collections import Traversal, Flow
 lst: Iterable[int] = [[1, 2, 3]]
 x = Flow(lst)[Traversal.flatten_if(lambda _: isinstance(_, list))]
 assert isinstance(x.unbox, Generator) and list(x.unbox) == [1, 2, 3]
+
+from Redy.Collections import Traversal, Flow
+lst: Iterable[int] = [0, 1, 2, 3, 4, 5, 6]
+x = Flow(lst)[Traversal.chunk_by(lambda x: x // 3)]
+assert list(x.unbox) == [[0, 1, 2], [3, 4, 5], [6]]
+x = Flow([])[Traversal.chunk_by(lambda x: x)]
+assert list(x.unbox) == []
+
+from Redy.Collections import Traversal, Flow
+x = [1, 1, 2]
+assert Flow(x)[Traversal.chunk][list].unbox == [[1, 1], [2]]
+assert Flow([])[Traversal.chunk][list].unbox == []
+
+from Redy.Collections import Flow, Traversal
+x = [1, 1, 2]
+Flow(x)[Traversal.group].unbox
+
+from Redy.Collections import Flow, Traversal
+x = [1, '1', 1.0]
+Flow(x)[Traversal.group_by(type)]
